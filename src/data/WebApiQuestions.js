4baf3b15243b8ce962b5c8afa98ff007bbadf015
GermanGiz <gizatullin-2007@mail.ru>
2023-11-05 00:08:14 +0300

    Полготовка к WebApi вопросам

--- src/data/WebApiQuestions.js
+++ src/data/WebApiQuestions.js
@@ -8,6 +8,29 @@ import QUESTION_7 from '../components/WebApiQuestions/Question_7/Question'
 import QUESTION_8 from '../components/WebApiQuestions/Question_8/Question'
 import QUESTION_9 from '../components/WebApiQuestions/Question_9/Question'
 import QUESTION_10 from '../components/WebApiQuestions/Question_10/Question'
+import QUESTION_11 from '../components/WebApiQuestions/Question_11/Question'
+import QUESTION_12 from '../components/WebApiQuestions/Question_12/Question'
+import QUESTION_13 from '../components/WebApiQuestions/Question_13/Question'
+import QUESTION_14 from '../components/WebApiQuestions/Question_14/Question'
+import QUESTION_15 from '../components/WebApiQuestions/Question_15/Question'
+import QUESTION_16 from '../components/WebApiQuestions/Question_16/Question'
+import QUESTION_17 from '../components/WebApiQuestions/Question_17/Question'
+import QUESTION_18 from '../components/WebApiQuestions/Question_18/Question'
+import QUESTION_19 from '../components/WebApiQuestions/Question_19/Question'
+import QUESTION_20 from '../components/WebApiQuestions/Question_20/Question'
+import QUESTION_21 from '../components/WebApiQuestions/Question_21/Question'
+import QUESTION_22 from '../components/WebApiQuestions/Question_22/Question'
+import QUESTION_23 from '../components/WebApiQuestions/Question_23/Question'
+import QUESTION_24 from '../components/WebApiQuestions/Question_24/Question'
+import QUESTION_25 from '../components/WebApiQuestions/Question_25/Question'
+import QUESTION_26 from '../components/WebApiQuestions/Question_26/Question'
+import QUESTION_27 from '../components/WebApiQuestions/Question_27/Question'
+import QUESTION_28 from '../components/WebApiQuestions/Question_28/Question'
+import QUESTION_29 from '../components/WebApiQuestions/Question_29/Question'
+import QUESTION_30 from '../components/WebApiQuestions/Question_30/Question'
+import QUESTION_31 from '../components/WebApiQuestions/Question_31/Question'
+import QUESTION_32 from '../components/WebApiQuestions/Question_32/Question'
+import QUESTION_33 from '../components/WebApiQuestions/Question_33/Question'
 
 export const WebApiQuestions = [
 	{
@@ -59,5 +82,120 @@ export const WebApiQuestions = [
 		id: 10,
 		question: 'Разница между протоколами TCP и UDP?',
 		answer: <QUESTION_10 />,
+	},
+	{
+		id: 11,
+		question: 'Что такое WebSocket? В чем принцип его работы?',
+		answer: <QUESTION_11 />,
+	},
+	{
+		id: 12,
+		question: 'Разница между Long-Polling, Websockets и Server-Sent Events?',
+		answer: <QUESTION_12 />,
+	},
+	{
+		id: 13,
+		question: 'Как работает JSONP?',
+		answer: <QUESTION_13 />,
+	},
+	{
+		id: 14,
+		question: 'Что такое IndexedDB в браузере? Преимущества IndexedDB?',
+		answer: <QUESTION_14 />,
+	},
+	{
+		id: 15,
+		question: 'Что такое Service Workers?',
+		answer: <QUESTION_15 />,
+	},
+	{
+		id: 16,
+		question: 'Что такое Web Workers?',
+		answer: <QUESTION_16 />,
+	},
+	{
+		id: 17,
+		question: 'Что такое Web Worklet?',
+		answer: <QUESTION_17 />,
+	},
+	{
+		id: 18,
+		question: 'Что такое SSL/TLS? Зачем они используются в веб-разработке?',
+		answer: <QUESTION_18 />,
+	},
+	{
+		id: 19,
+		question: 'Механизм устанавки сеанса между клиентом и сервером?',
+		answer: <QUESTION_19 />,
+	},
+	{
+		id: 20,
+		question: 'Что Такое API?',
+		answer: <QUESTION_20 />,
+	},
+	{
+		id: 21,
+		question: 'Что такое CDN?',
+		answer: <QUESTION_21 />,
+	},
+	{
+		id: 22,
+		question: 'Что такое IP-адрес?',
+		answer: <QUESTION_22 />,
+	},
+	{
+		id: 23,
+		question: 'Разница между host и domain?',
+		answer: <QUESTION_23 />,
+	},
+	{
+		id: 24,
+		question: 'Разница между URI и URL?',
+		answer: <QUESTION_24 />,
+	},
+	{
+		id: 25,
+		question: 'Почему очищать кэш важно? Как это можно сделать?',
+		answer: <QUESTION_25 />,
+	},
+	{
+		id: 26,
+		question: 'Разница между идентификацией, аутентификацией, авторизацией?',
+		answer: <QUESTION_26 />,
+	},
+	{
+		id: 27,
+		question: 'Виды аутентификации?',
+		answer: <QUESTION_27 />,
+	},
+	{
+		id: 28,
+		question: 'Что такое безопасные (Secure) и HttpOnly cookies?',
+		answer: <QUESTION_28 />,
+	},
+	{
+		id: 29,
+		question: 'Что такое Content Security Policy (CSP)?',
+		answer: <QUESTION_29 />,
+	},
+	{
+		id: 30,
+		question: 'Что такое CORS?',
+		answer: <QUESTION_30 />,
+	},
+	{
+		id: 31,
+		question: 'Что такое межсайтовый скриптинг (XSS)?',
+		answer: <QUESTION_31 />,
+	},
+	{
+		id: 32,
+		question: 'Методы повышения безопасности веб-приложений?',
+		answer: <QUESTION_32 />,
+	},
+	{
+		id: 33,
+		question: 'Что такое OWASP Top 10?',
+		answer: <QUESTION_33 />,
 	},
 ]
